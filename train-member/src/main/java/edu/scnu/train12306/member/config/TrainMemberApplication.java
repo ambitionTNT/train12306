7d4aa30fdb70f14120540b2300ebdb07a11d3ce6
ambition <1292217724@qq.com>
2023-12-04 15:17:46 +0800

    4.4 修改启动日志，增加/member，修改logback

--- train-member/src/main/java/edu/scnu/train12306/member/config/TrainMemberApplication.java
+++ train-member/src/main/java/edu/scnu/train12306/member/config/TrainMemberApplication.java
@@ -1,8 +1,10 @@
 package edu.scnu.train12306.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author long
@@ -13,8 +15,13 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("edu.scnu.train12306")
+@Slf4j
 public class TrainMemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TrainMemberApplication.class, args);
+        SpringApplication application = new SpringApplication(TrainMemberApplication.class);
+        ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        log.info("启动成功");
+
+        log.info("地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 }
