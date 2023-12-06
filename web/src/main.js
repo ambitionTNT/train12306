5b83919159a69e936458acee886f40fe0db5e43d
ambition <1292217724@qq.com>
2023-12-06 22:36:20 +0800

    5-4 集成ant design vue 图标库

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
+const app = createApp(App)
+app.use(store).use(router).use(Antd).mount('#app')
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>Add</a-button><down-square-outlined />
   </div>
 </template>
