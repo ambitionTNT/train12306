df4dbbdda008c0d46e9bcb327a30628bb6f41b8c
ambition <1292217724@qq.com>
2023-12-09 10:36:18 +0800

    5-10 增加 axios拦截器，打印请求参数和响应结果信息

--- web/src/main.js
+++ web/src/main.js
@@ -5,9 +5,31 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+
+
+
+
+
 const app = createApp(App)
 app.use(store).use(router).use(Antd).mount('#app')
 const icons = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数:',config);
+    return config
+}, error => {
+    return Promise.reject(error)
+});
+
+axios.interceptors.response.use(function (response){
+    console.log('返回结果:',response);
+    return response
+},error => {
+    console.log('返回的错误:',error);
+
+    return Promise.reject(error)
+})
