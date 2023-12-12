b5fcfd0d39a4fa348b953e216c45ea4a4b02d2b7
ambition <1292217724@qq.com>
2023-12-12 15:38:06 +0800

    7-10 对返回的日期格式化

--- train-member/src/main/java/edu/scnu/train12306/member/resp/PassengerQueryResp.java
+++ train-member/src/main/java/edu/scnu/train12306/member/resp/PassengerQueryResp.java
@@ -1,5 +1,7 @@
 package edu.scnu.train12306.member.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class PassengerQueryResp {
@@ -17,9 +19,10 @@ public class PassengerQueryResp {
     private String type;
 
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
 
