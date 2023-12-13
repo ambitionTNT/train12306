f85d6c8b9687b3eb747551d39a8f2d64c5e8fd3d
ambition <1292217724@qq.com>
2023-12-13 08:52:18 +0800

    7-16 更改package.js文件要重启，将我枚举数组放到单独文件中

---
+++ web/public/js/enums.js
@@ -0,0 +1,3 @@
+PASSENGER_TYPE_ARRAY = [ {key: "1", value: "成人1"},
+        {key: "2", value: "儿童2"},
+        {key: "3", value: "学生3"},];
--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -123,10 +123,7 @@ export default defineComponent({
         key: 'operation',
       }
     ]);
-    const PASSENGER_TYPE_ARRAY =
-       [ {key: "1", value: "成人1"},
-        {key: "2", value: "儿童2"},
-        {key: "3", value: "学生3"},]
+    const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
 
 
     const passenger = ref({
